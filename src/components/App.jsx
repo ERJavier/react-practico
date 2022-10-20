5d4eef06f74e3a103b6c345fba6fa2822f568494
Javier Cepeda <erjavierc@gmail.com>
2022-10-20 09:45:30 -0400

    Sass config ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import '../../styles/global.scss'
+import '../styles/global.scss'
 const App = () => {
     return (
         <h1> Hola Mundo, bienvenidos al curso practico </h1>
